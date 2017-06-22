f3d117e7bb28c2ead17dc80072b4897aa184166b
Ole Tange <ole@tange.dk>
2017-06-22 23:14:32 +0200

    parallel: Fixed bug #51293: parallel does not preserve symlinked directory structure on remote.

--- testsuite/tests-to-run/parallel-local-ssh6.sh
+++ testsuite/tests-to-run/parallel-local-ssh6.sh
@@ -1,87 +1,126 @@
 #!/bin/bash
 
-SSHLOGIN1=parallel@lo
-SSHLOGIN2=csh@lo
+# At most 2 parallel sshs per function
+
+export SSHLOGIN1=parallel@lo
+export SSHLOGIN2=csh@lo
 mkdir -p tmp
 
-# -L1 will join lines ending in ' '
-cat <<'EOF' | sed -e s/\$SERVER1/$SERVER1/\;s/\$SERVER2/$SERVER2/\;s/\$SSHLOGIN1/$SSHLOGIN1/\;s/\$SSHLOGIN2/$SSHLOGIN2/ | parallel -vj5 -k --joblog /tmp/jl-`basename $0` -L1
-echo '### Test --onall'; 
-  parallel --onall --tag -k -S $SSHLOGIN1,$SSHLOGIN2 '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2
+par_test_onall() {
+    echo '### Test --onall'
+    parallel --onall --tag -k -S $SSHLOGIN1,$SSHLOGIN2 '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2
+}
 
-echo '### Test | --onall'; 
-  seq 3 | parallel --onall --tag -k -S $SSHLOGIN1,$SSHLOGIN2 '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c :::: -
+par_test_pipe_onall() {
+    echo '### Test | --onall'
+    seq 3 |
+	parallel --onall --tag -k -S $SSHLOGIN1,$SSHLOGIN2 '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c :::: -
+}
 
-echo '### Test --onall -u'; 
-  parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 -u '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3 | sort
+par_test_onall_u() {
+    echo '### Test --onall -u'
+    parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 -u '(echo {1} {2}) | awk \{print\ \$2}' ::: a b c ::: 1 2 3 |
+	sort
+}    
 
-echo '### Test --nonall'; 
-  parallel --nonall -k -S $SSHLOGIN1,$SSHLOGIN2 pwd | sort
+par_test_nonall() {
+    echo '### Test --nonall'
+    parallel --nonall -k -S $SSHLOGIN1,$SSHLOGIN2 pwd |
+	sort
+}    
 
-echo '### Test --nonall -u - should be interleaved x y x y'; 
-  parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 -u 'pwd|grep -q csh && sleep 3; pwd;sleep 12;pwd;'
+par_test_nonall_u() {
+    echo '### Test --nonall -u - should be interleaved x y x y'
+    parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 -u 'pwd|grep -q csh && sleep 3; pwd;sleep 12;pwd;'
+}    
 
-echo '### Test read sshloginfile from STDIN'; 
-  echo $SSHLOGIN1,$SSHLOGIN2 | parallel -S - -k --nonall pwd; 
-  echo $SSHLOGIN1,$SSHLOGIN2 | parallel --sshloginfile - -k --onall pwd\; echo ::: foo
+par_read_sshloginfile_from_stdin() {
+    echo '### Test read sshloginfile from STDIN'
+    echo $SSHLOGIN1,$SSHLOGIN2 | parallel -S - -k --nonall pwd
+    echo $SSHLOGIN1,$SSHLOGIN2 | parallel --sshloginfile - -k --onall pwd\; echo ::: foo
+}
 
-echo '**'
+par_nonall_basefile() {
+    echo '### Test --nonall --basefile'
+    touch tmp/nonall--basefile
+    stdout parallel --nonall --basefile tmp/nonall--basefile -S $SSHLOGIN1,$SSHLOGIN2 ls tmp/nonall--basefile
+    stdout parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 rm tmp/nonall--basefile
+    stdout rm tmp/nonall--basefile
+}
 
-echo '### Test --nonall --basefile'; 
-  touch tmp/nonall--basefile; 
-  stdout parallel --nonall --basefile tmp/nonall--basefile -S $SSHLOGIN1,$SSHLOGIN2 ls tmp/nonall--basefile; 
-  stdout parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 rm tmp/nonall--basefile; 
-  stdout rm tmp/nonall--basefile
+par_onall_basefile() {
+    echo '### Test --onall --basefile'
+    touch tmp/onall--basefile
+    stdout parallel --onall --basefile tmp/onall--basefile -S $SSHLOGIN1,$SSHLOGIN2 ls {} ::: tmp/onall--basefile
+    stdout parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 rm {} ::: tmp/onall--basefile
+    stdout rm tmp/onall--basefile
+}    
 
-echo '**'
+par_nonall_basefile_cleanup() {
+    echo '### Test --nonall --basefile --cleanup (rm should fail)'
+    touch tmp/nonall--basefile--clean
+    stdout parallel --nonall --basefile tmp/nonall--basefile--clean --cleanup -S $SSHLOGIN1,$SSHLOGIN2 ls tmp/nonall--basefile--clean
+    stdout parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 rm tmp/nonall--basefile--clean
+    stdout rm tmp/nonall--basefile--clean
+}
 
-echo '### Test --onall --basefile'; 
-  touch tmp/onall--basefile; 
-  stdout parallel --onall --basefile tmp/onall--basefile -S $SSHLOGIN1,$SSHLOGIN2 ls {} ::: tmp/onall--basefile; 
-  stdout parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 rm {} ::: tmp/onall--basefile; 
-  stdout rm tmp/onall--basefile
+par_onall_basefile_cleanup() {
+    echo '### Test --onall --basefile --cleanup (rm should fail)'
+    touch tmp/onall--basefile--clean
+    stdout parallel --onall --basefile tmp/onall--basefile--clean --cleanup -S $SSHLOGIN1,$SSHLOGIN2 ls {} ::: tmp/onall--basefile--clean
+    stdout parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 rm {} ::: tmp/onall--basefile--clean
+    stdout rm tmp/onall--basefile--clean
+}
 
-echo '**'
+par_workdir_dot() {
+    echo '### Test --workdir .'
+    ssh $SSHLOGIN1 mkdir -p mydir
+    mkdir -p $HOME/mydir
+    cd $HOME/mydir
+    parallel --workdir . -S $SSHLOGIN1 ::: pwd
+}
 
-echo '### Test --nonall --basefile --cleanup (rm should fail)'; 
-  touch tmp/nonall--basefile--clean; 
-  stdout parallel --nonall --basefile tmp/nonall--basefile--clean --cleanup -S $SSHLOGIN1,$SSHLOGIN2 ls tmp/nonall--basefile--clean; 
-  stdout parallel --nonall -S $SSHLOGIN1,$SSHLOGIN2 rm tmp/nonall--basefile--clean; 
-  stdout rm tmp/nonall--basefile--clean
+par_wd_dot() {
+    echo '### Test --wd .'
+    ssh $SSHLOGIN2 mkdir -p mydir
+    mkdir -p $HOME/mydir
+    cd $HOME/mydir
+    parallel --workdir . -S $SSHLOGIN2 ::: pwd
+}    
 
-echo '**'
+par_wd_braces() {
+    echo '### Test --wd {}'
+    ssh $SSHLOGIN2 rm -rf wd1 wd2
+    mkdir -p $HOME/mydir
+    cd $HOME/mydir
+    parallel --workdir {} -S $SSHLOGIN2 touch ::: wd1 wd2
+    ssh $SSHLOGIN2 ls -d wd1 wd2
+}
 
-echo '### Test --onall --basefile --cleanup (rm should fail)'; 
-  touch tmp/onall--basefile--clean; 
-  stdout parallel --onall --basefile tmp/onall--basefile--clean --cleanup -S $SSHLOGIN1,$SSHLOGIN2 ls {} ::: tmp/onall--basefile--clean; 
-  stdout parallel --onall -S $SSHLOGIN1,$SSHLOGIN2 rm {} ::: tmp/onall--basefile--clean; 
-  stdout rm tmp/onall--basefile--clean
+par_wd_perlexpr() {
+    echo '### Test --wd {= =}'
+    ssh $SSHLOGIN2 rm -rf WD1 WD2
+    mkdir -p $HOME/mydir
+    cd $HOME/mydir
+    parallel --workdir '{= $_=uc($_) =}' -S $SSHLOGIN2 touch ::: wd1 wd2
+    ssh $SSHLOGIN2 ls -d WD1 WD2
+}
 
-echo '**'
+par_nonall_wd() {
+    echo '### Test --nonall --wd'
+    parallel --workdir /tmp -S $SSHLOGIN2 --nonall pwd
+}
 
-echo '### Test --workdir .'; 
-  ssh $SSHLOGIN1 mkdir -p mydir; 
-  mkdir -p $HOME/mydir; cd $HOME/mydir; 
-  parallel --workdir . -S $SSHLOGIN1 ::: pwd
+par_remote_symlink_dir() {
+    echo 'bug #51293: parallel does not preserve symlinked directory structure on remote'
+    ssh parallel@lo 'mkdir -p tmp; rm -rf wd; ln -s tmp wd'
+    mkdir -p wd
+    touch wd/testfile
+    parallel --nonall -S parallel@lo --basefile wd/testfile
+    ssh parallel@lo rm wd && echo OK: wd is still a symlink
+}
 
-echo '### Test --wd .'; 
-  ssh $SSHLOGIN2 mkdir -p mydir; 
-  mkdir -p $HOME/mydir; cd $HOME/mydir; 
-  parallel --workdir . -S $SSHLOGIN2 ::: pwd
-
-echo '### Test --wd {}'; 
-  ssh $SSHLOGIN2 rm -rf wd1 wd2; 
-  mkdir -p $HOME/mydir; cd $HOME/mydir; 
-  parallel --workdir {} -S $SSHLOGIN2 touch ::: wd1 wd2; 
-  ssh $SSHLOGIN2 ls -d wd1 wd2
-
-echo '### Test --wd {= =}'; 
-  ssh $SSHLOGIN2 rm -rf WD1 WD2; 
-  mkdir -p $HOME/mydir; cd $HOME/mydir; 
-  parallel --workdir '{= $_=uc($_) =}' -S $SSHLOGIN2 touch ::: wd1 wd2; 
-  ssh $SSHLOGIN2 ls -d WD1 WD2
-
-echo '### Test --nonall --wd'; 
-  parallel --workdir /tmp -S $SSHLOGIN2 --nonall pwd
-
-EOF
+export -f $(compgen -A function | grep par_)
+#compgen -A function | grep par_ | sort | parallel --delay $D -j$P --tag -k '{} 2>&1'
+compgen -A function | grep par_ | sort |
+    parallel --joblog /tmp/jl-`basename $0` -j5 --tag -k '{} 2>&1'
